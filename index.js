81587f6ff96ea1b128a97afd26cdbe88e9888e9e
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-05-11 03:25:47 +0530

    update index.js

--- index.js
+++ index.js
@@ -23,26 +23,27 @@ app.get('/', (req, res) => {
 })
 
 //! Routes Setup
-const usersRoute = require('./routes/users-routes'),
-    todosRoute = require('./routes/todos-routes'),
-    photosRoute = require('./routes/photos-routes'),
-    postsRoute = require('./routes/posts-routes'),
-    commentsRoute = require('./routes/comments-routes'),
-    productsRoutes = require('./routes/products-routes'),
-    ordersRoutes = require('./routes/orders-routes'),
-    eventsRoute = require('./routes/events-routes'),
-    employeesRoute = require('./routes/employees-routes'),
-    inventoryRoute = require('./routes/inventory-routes')
-app.use(usersRoute)
-app.use(todosRoute)
-app.use(photosRoute)
-app.use(postsRoute)
-app.use(commentsRoute)
-app.use(productsRoutes)
-app.use(ordersRoutes)
-app.use(eventsRoute)
+const employeesRoute = require('./routes/employees-routes')
+// usersRoute = require('./routes/users-routes'),
+// todosRoute = require('./routes/todos-routes'),
+// photosRoute = require('./routes/photos-routes'),
+// postsRoute = require('./routes/posts-routes'),
+// commentsRoute = require('./routes/comments-routes'),
+// productsRoutes = require('./routes/products-routes'),
+// ordersRoutes = require('./routes/orders-routes'),
+// eventsRoute = require('./routes/events-routes'),
+// inventoryRoute = require('./routes/inventory-routes')
+
 app.use(employeesRoute)
-app.use(inventoryRoute)
+// app.use(usersRoute)
+// app.use(todosRoute)
+// app.use(photosRoute)
+// app.use(postsRoute)
+// app.use(commentsRoute)
+// app.use(productsRoutes)
+// app.use(ordersRoutes)
+// app.use(eventsRoute)
+// app.use(inventoryRoute)
 
 //! Server Setup
 app.listen(PORT, () => {
